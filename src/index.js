c7e01bdfcd79b63518a9c587bb2ac53d88934f7b
joaopedrodcf <joaoferr93@gmail.com>
2018-09-03 01:38:01 +0100

    feat: Add eslint config

---
+++ src/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        'jest/globals': true
+    },
+    extends: [
+        'airbnb',
+        'prettier',
+        'prettier/react',
+        'plugin:jest/recommended'
+    ],
+    plugins: ['react', 'jsx-a11y', 'import', 'prettier', 'jest'],
+    rules: {
+        'react/jsx-filename-extension': 0,
+        'jsx-a11y/anchor-is-valid': [
+            'error',
+            {
+                components: ['Link'],
+                specialLink: ['to']
+            }
+        ],
+        'prettier/prettier': ['error']
+    }
+};
